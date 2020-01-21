a74991288169866ec9c0a01d48db27a6d5eaba09
Calin Crisan <ccrisan@gmail.com>
2020-01-21 21:15:27 +0200

    Widgets/NumericInput: trigger change on scroll

--- js/widgets/common-widgets/numeric-input.js
+++ js/widgets/common-widgets/numeric-input.js
@@ -1,8 +1,30 @@
 
 import $ from '$qui/lib/jquery.module.js'
 
+import {asap} from '$qui/utils/misc.js'
+
 import * as TextInput from './text-input.js' /* Needed */
 
 
 // TODO do not allow user input of non-numeric characters
-$.widget('qui.numericinput', $.qui.textinput, {type: 'number'})
+$.widget('qui.numericinput', $.qui.textinput, {
+
+    type: 'number',
+
+    _create: function () {
+        this._super('_create')
+
+        /* Trigger change event on widget element when scrolling, but only if focused */
+        this._input.on('mousewheel', function (e) {
+            if (!this._input.is(':focus')) {
+                e.preventDefault()
+                return
+            }
+
+            asap(function () {
+                this.element.trigger('change')
+            }.bind(this))
+        }.bind(this))
+    }
+
+})
