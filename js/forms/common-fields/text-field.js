e35bbf2d092dd8006f2ff22e8a06df3763ca2e23
Calin Crisan <ccrisan@gmail.com>
2020-04-13 18:23:13 +0300

    Firms/TextField: add pattern validation

--- js/forms/common-fields/text-field.js
+++ js/forms/common-fields/text-field.js
@@ -1,4 +1,6 @@
 
+import {gettext}        from '$qui/base/i18n.js'
+
 import JQueryUIField from './jquery-ui-field.js'
 
 
@@ -16,6 +18,7 @@ class TextField extends JQueryUIField {
      * @param {Boolean} [autocomplete] enables or disables field autocomplete (enabled by default)
      * @param {?Number} [minLength] minimum required text length
      * @param {?Number} [maxLength] maximum allowed text length
+     * @param {?String} [pattern] a regular expression used to validate the value
      * @param {Boolean} [continuousChange] set to `false` to prevent triggering change events at each key stroke
      * @param {Object} [widgetAttrs] extra attributes to pass to underlying JQueryUI widget
      * @param {...*} args parent class parameters
@@ -26,6 +29,7 @@ class TextField extends JQueryUIField {
         autocomplete = true,
         minLength = null,
         maxLength = null,
+        pattern = null,
         continuousChange = true,
         widgetAttrs = {},
         ...args
@@ -40,6 +44,27 @@ class TextField extends JQueryUIField {
         })
 
         super({widgetAttrs: widgetAttrs, ...args})
+
+        this._pattern = pattern
+    }
+
+    validateWidget(value) {
+        let error = super.validateWidget(value)
+        if (error) {
+            return error
+        }
+
+        if (!this.isRequired() && !value) {
+            return
+        }
+
+        /* Apply pattern validation, if supplied */
+        if (this._pattern) {
+            let match = value.match(new RegExp(this._pattern))
+            if (match == null) {
+                return gettext('Value does not match required pattern.')
+            }
+        }
     }
 
     getInputElement() {
