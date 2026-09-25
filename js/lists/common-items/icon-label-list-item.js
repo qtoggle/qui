5bc5a10308732eadfec5be7267148b53ba5a14cc
Calin Crisan <ccrisan@gmail.com>
2026-09-25 17:49:42 +0300

    lists: Leave unchanged labels and icons alone on in-place updates

--- js/lists/common-items/icon-label-list-item.js
+++ js/lists/common-items/icon-label-list-item.js
@@ -45,9 +45,19 @@ class IconLabelListItem extends mix(ListItem).with(IconLabelViewMixin) {
             return false
         }
 
-        this.setLabel(other.getLabel())
-        this.setSubLabel(other.getSubLabel())
-        this.setIcon(other.getIcon())
+        /* Updates arrive for every server event, mostly with nothing changed. Re-applying an icon rebuilds its elements
+         * with a fade and may sample its colors, so each part is only set when it differs. */
+        if (other.getLabel() !== this.getLabel()) {
+            this.setLabel(other.getLabel())
+        }
+        if (other.getSubLabel() !== this.getSubLabel()) {
+            this.setSubLabel(other.getSubLabel())
+        }
+
+        let icon = other.getIcon()
+        if (icon ? !icon.equals(this.getIcon()) : this.getIcon()) {
+            this.setIcon(icon)
+        }
 
         return true
     }
